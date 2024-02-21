a6d766b85e30c43d2a464ca355bc558e781a3cd3
marwal1987 <marwal1987@gmail.com>
2024-02-21 10:57:19 +0100

    added utils/fetchNews & components folder

---
+++ src/utils/fetchNews.js
@@ -0,0 +1,11 @@
+export default async function fetchNews(apiKey, options) {
+  const res = await fetch(
+    `https://newsdata.io/api/1/news?apikey=${apiKey}&q=${options}`
+  );
+  console.log(
+    "request:",
+    `https://newsdata.io/api/1/news?apikey=${apiKey}&q=${options}`
+  );
+  const data = await res.json();
+  return data.results || [];
+}
