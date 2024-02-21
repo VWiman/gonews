b87c8858ba41da7e3696b72b4c2e34e78d0bc826
marwal1987 <marwal1987@gmail.com>
2024-02-21 11:49:10 +0100

    make sure to render only first 500 charactersof description

--- src/pages/index.js
+++ src/pages/index.js
@@ -5,54 +5,57 @@ import fetchNews from "@/utils/fetchNews";
 
 // definiera en funktionell komponent Home, som tar emot nyhetsdata som en prop
 const Home = ({ news }) => {
-	//hämta bokmärken från Redux-store och lagra dem i variabeln bookmarks
-	const bookmarks = useSelector((state) => state.bookmarks);
+  //hämta bokmärken från Redux-store och lagra dem i variabeln bookmarks
+  const bookmarks = useSelector((state) => state.bookmarks);
 
-	// hook för att få en actionfunktion från Redux och lagra den i variabeln dispatch
-	const dispatch = useDispatch();
+  // hook för att få en actionfunktion från Redux och lagra den i variabeln dispatch
+  const dispatch = useDispatch();
 
-	// hantera klickhändelsen för att lägga till eller ta bort ett bokmärke.
-	const handleBookmarkToggle = (article) => {
-		dispatch(toggleBookmark(article));
-	};
+  // hantera klickhändelsen för att lägga till eller ta bort ett bokmärke.
+  const handleBookmarkToggle = (article) => {
+    dispatch(toggleBookmark(article));
+  };
 
-	// rendera nyhetsartiklarna
-	return (
-		<div>
-			<h1>Huvudnyheter</h1>
-			<ul>
-				{news.map((article) => (
-					<li key={article.article_id}>
-						{article.title}
-						<button onClick={() => handleBookmarkToggle(article)}>
-							{bookmarks.some((bookmark) => bookmark.article_id === article.article_id)
-								? "Ta bort bokmärke"
-								: "Lägg till bokmärke"}
-						</button>
-					</li>
-				))}
-			</ul>
-		</div>
-	);
+  // rendera nyhetsartiklarna
+  return (
+    <div>
+      <h1>Huvudnyheter</h1>
+      <ul>
+        {news.map((article) => (
+          <li key={article.article_id}>
+            <h2 className="text-2xl">{article.title}</h2>
+            <p className="text-lg">{article.description.slice(0, 500)}</p>
+            <button onClick={() => handleBookmarkToggle(article)}>
+              {bookmarks.some(
+                (bookmark) => bookmark.article_id === article.article_id
+              )
+                ? "Ta bort bokmärke"
+                : "Lägg till bokmärke"}
+            </button>
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
 };
 
 // funktion för att hämta nyhetsdata vid build samt var 10 min.
 export async function getStaticProps() {
-	const apiKey = process.env.API_KEY;
-	const options = `size=10&language=en`;
-	try {
-		const news = await fetchNews(apiKey, options);
-		const refreshTime = 60 * 10;
-		return {
-			props: { news },
-			revalidate: refreshTime,
-		};
-	} catch (error) {
-		console.error("Error fetching news:", error);
-		return {
-			props: { news: [] },
-		};
-	}
+  const apiKey = process.env.API_KEY;
+  const options = `size=10&language=en`;
+  try {
+    const news = await fetchNews(apiKey, options);
+    const refreshTime = 60 * 10;
+    return {
+      props: { news },
+      revalidate: refreshTime,
+    };
+  } catch (error) {
+    console.error("Error fetching news:", error);
+    return {
+      props: { news: [] },
+    };
+  }
 }
 
 export default Home;
