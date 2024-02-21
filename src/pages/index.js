cceecbceb284e802baba53378243ce6fff94eb90
marwal1987 <marwal1987@gmail.com>
2024-02-21 10:28:47 +0100

    added comments

--- src/pages/index.js
+++ src/pages/index.js
@@ -2,14 +2,20 @@ import React from "react";
 import { useSelector, useDispatch } from "react-redux";
 import { toggleBookmark } from "../store/bookmarkSlice";
 
+// definiera en funktionell komponent Home, som tar emot nyhetsdata som en prop
 const Home = ({ news }) => {
+  //hämta bokmärken från Redux-store och lagra dem i variabeln bookmarks
   const bookmarks = useSelector((state) => state.bookmarks);
+
+  // hook för att få en actionfunktion från Redux och lagra den i variabeln dispatch
   const dispatch = useDispatch();
 
+  // hantera klickhändelsen för att lägga till eller ta bort ett bokmärke.
   const handleBookmarkToggle = (article) => {
     dispatch(toggleBookmark(article));
   };
 
+  // rendera nyhetsartiklarna
   return (
     <div>
       <h1>Huvudnyheter</h1>
@@ -31,6 +37,7 @@ const Home = ({ news }) => {
   );
 };
 
+// funktion för att hämta nyhetsdata från API:et varje gång sidan renderas på servern.
 export const getServerSideProps = async () => {
   try {
     const response = await fetch("https://api.example.com/news");
