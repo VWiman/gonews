84b48c72d4563be55363fde777a69c136ce3b258
why_code <119089018+VWiman@users.noreply.github.com>
2024-02-21 11:06:44 +0100

    Changed to static props and implemented real url path

--- src/pages/index.js
+++ src/pages/index.js
@@ -4,54 +4,54 @@ import { toggleBookmark } from "../store/bookmarkSlice";
 
 // definiera en funktionell komponent Home, som tar emot nyhetsdata som en prop
 const Home = ({ news }) => {
-  //hämta bokmärken från Redux-store och lagra dem i variabeln bookmarks
-  const bookmarks = useSelector((state) => state.bookmarks);
+	//hämta bokmärken från Redux-store och lagra dem i variabeln bookmarks
+	const bookmarks = useSelector((state) => state.bookmarks);
 
-  // hook för att få en actionfunktion från Redux och lagra den i variabeln dispatch
-  const dispatch = useDispatch();
+	// hook för att få en actionfunktion från Redux och lagra den i variabeln dispatch
+	const dispatch = useDispatch();
 
-  // hantera klickhändelsen för att lägga till eller ta bort ett bokmärke.
-  const handleBookmarkToggle = (article) => {
-    dispatch(toggleBookmark(article));
-  };
+	// hantera klickhändelsen för att lägga till eller ta bort ett bokmärke.
+	const handleBookmarkToggle = (article) => {
+		dispatch(toggleBookmark(article));
+	};
 
-  // rendera nyhetsartiklarna
-  return (
-    <div>
-      <h1>Huvudnyheter</h1>
-      <ul>
-        {news.map((article) => (
-          <li key={article.article_id}>
-            {article.title}
-            <button onClick={() => handleBookmarkToggle(article)}>
-              {bookmarks.some(
-                (bookmark) => bookmark.article_id === article.article_id
-              )
-                ? "Ta bort bokmärke"
-                : "Lägg till bokmärke"}
-            </button>
-          </li>
-        ))}
-      </ul>
-    </div>
-  );
+	// rendera nyhetsartiklarna
+	return (
+		<div>
+			<h1>Huvudnyheter</h1>
+			<ul>
+				{news.map((article) => (
+					<li key={article.article_id}>
+						{article.title}
+						<button onClick={() => handleBookmarkToggle(article)}>
+							{bookmarks.some((bookmark) => bookmark.article_id === article.article_id)
+								? "Ta bort bokmärke"
+								: "Lägg till bokmärke"}
+						</button>
+					</li>
+				))}
+			</ul>
+		</div>
+	);
 };
 
 // funktion för att hämta nyhetsdata från API:et varje gång sidan renderas på servern.
-export const getServerSideProps = async () => {
-  try {
-    const response = await fetch("https://api.example.com/news");
-    const news = await response.json();
-
-    return {
-      props: { news },
-    };
-  } catch (error) {
-    console.error("Error fetching news:", error);
-    return {
-      props: { news: [] },
-    };
-  }
-};
+export async function getStaticProps() {
+	const apiKey = process.env.API_KEY;
+	const options = `size=10&language=en`;
+	try {
+		const news = await fetchNews(apiKey, options);
+		const refreshTime = 60 * 10;
+		return {
+			props: { news },
+		};
+	} catch (error) {
+		console.error("Error fetching news:", error);
+		return {
+			props: { news: [] },
+			revalidate: refreshTime,
+		};
+	}
+}
 
 export default Home;
--- src/utils/fetchNews.js
+++ src/utils/fetchNews.js
@@ -1,11 +1,7 @@
 export default async function fetchNews(apiKey, options) {
-  const res = await fetch(
-    `https://newsdata.io/api/1/news?apikey=${apiKey}&q=${options}`
-  );
-  console.log(
-    "request:",
-    `https://newsdata.io/api/1/news?apikey=${apiKey}&q=${options}`
-  );
-  const data = await res.json();
-  return data.results || [];
+	const res = await fetch(`https://newsdata.io/api/1/news?apikey=${apiKey}&q=${options}`);
+	console.log("request:", `https://newsdata.io/api/1/news?apikey=${apiKey}&q=${options}`);
+	const data = await res.json();
+
+	return data.results || [];
 }
