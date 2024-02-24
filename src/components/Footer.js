62c71c579a31becad146cf83bc15af79a5860e7a
ozayo <ozay.ozdemir@gmail.com>
2024-02-24 16:41:38 +0100

    basic footer comp

---
+++ src/components/Footer.js
@@ -0,0 +1,13 @@
+const Footer = () => {
+  return (
+    <>
+      <footer className="py-8 text-center mt-14">
+        <div className="text-slate-500 text-xs my-2 font-light">
+          <p>Chas React News</p> <p>2024 - GoNews - Chas Academy</p>
+        </div>
+      </footer>
+    </>
+  );
+};
+
+export default Footer;
