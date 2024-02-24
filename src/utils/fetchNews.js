159fcceaa2a9d89b0033fb1b8e8471c0b64d7cdf
why_code <119089018+VWiman@users.noreply.github.com>
2024-02-24 20:00:03 +0100

    Fixed an issue with how the api was being called. It should now fetch 10 news for each category.

--- src/utils/fetchNews.js
+++ src/utils/fetchNews.js
@@ -1,6 +1,6 @@
 export default async function fetchNews(apiKey, options) {
-	const res = await fetch(`https://newsdata.io/api/1/news?apikey=${apiKey}&q=${options}`);
-	console.log("request:", `https://newsdata.io/api/1/news?apikey=${apiKey}&q=${options}`);
+	const res = await fetch(`https://newsdata.io/api/1/news?apikey=${apiKey}&${options}`);
+	console.log("request:", `https://newsdata.io/api/1/news?apikey=${apiKey}&${options}`);
 	const data = await res.json();
 
 	return data.results || [];
