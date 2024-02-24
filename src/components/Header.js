9fc64fe21c63190796d173ea2ac47b74fab36f18
ozayo <ozay.ozdemir@gmail.com>
2024-02-24 19:10:24 +0100

    fixed mobile view

--- src/components/Header.js
+++ src/components/Header.js
@@ -16,74 +16,106 @@ const Header = () => {
     setIsMobileMenuOpen(!isMobileMenuOpen);
   };
 
+  const closeMobileMenu = () => {
+    setIsMobileMenuOpen(false);
+  };
+
   return (
-    <header className="flex justify-between items-center py-3">
-      {/* Logo */}
+    <header className="py-3">
+      <div className="flex justify-between items-center content-center">
+        {/* Logo */}
+        <div className="flex items-center">
+          <Link href="/">
+            <img
+              src={darkMode ? "/images/gonews-w.svg" : "/images/gonews.svg"}
+              alt="Logo"
+              className="h-9 w-auto mr-4"
+              width={140}
+            />
+          </Link>
+        </div>
 
-      <div className="flex items-center">
-        <Link href="/">
-          <img
-            src={darkMode ? "/images/gonews-w.svg" : "/images/gonews.svg"}
-            alt="Logo"
-            className="h-8 w-auto mr-4"
-            width={140}
-          />
-        </Link>
-      </div>
-      {/* Desktop navigation */}
-      <nav className="hidden md:flex space-x-4">
-        <Link href="/">Home</Link>
-        <Link href="/sports">Sport News</Link>
-        <Link href="/entertainment">Entertainment</Link>
-        <Link href="/bookmarks">Bokmärken</Link>
-      </nav>
+        {/* Desktop navigation */}
+        <nav className="hidden md:flex space-x-4">
+          <Link href="/">Home</Link>
+          <Link href="/sports">Sport News</Link>
+          <Link href="/entertainment">Entertainment</Link>
+          <Link href="/bookmarks">Bokmärken</Link>
+        </nav>
 
-      {/* Dark mode toggle */}
-      <div className="flex items-center">
-        <button
-          onClick={() => dispatch(toggleDarkMode())}
-          className="focus:outline-none mr-4"
-        >
-          {darkMode ? (
-            <IoMdSunny size="1.3em" className="text-white" />
-          ) : (
-            <IoMdMoon size="1.3em" className="text-black" />
-          )}
-        </button>
-      </div>
+        {/* Dark mode toggle */}
+        <div className="flex items-center">
+          <button
+            onClick={() => dispatch(toggleDarkMode())}
+            className="focus:outline-none mr-4"
+          >
+            {darkMode ? (
+              <IoMdSunny size="1.3em" className="text-white" />
+            ) : (
+              <IoMdMoon size="1.3em" className="text-black" />
+            )}
+          </button>
+        </div>
 
-      {/* Mobile navigation */}
-      <div className="md:hidden flex items-center">
-        <button onClick={toggleMobileMenu} className="focus:outline-none">
-          {/* Hamburger menu icon */}
-          {isMobileMenuOpen ? (
-            <FaTimes className="h-7 w-7" />
-          ) : (
-            <FaBars className="h-7 w-7" />
-          )}
-        </button>
+        {/* Mobile navigation */}
+        <div className="md:hidden flex items-center">
+          <button onClick={toggleMobileMenu} className="focus:outline-none">
+            {isMobileMenuOpen ? (
+              <FaTimes className="h-7 w-7" />
+            ) : (
+              <FaBars className="h-7 w-7" />
+            )}
+          </button>
+        </div>
       </div>
 
       {/* Mobile menu */}
       {isMobileMenuOpen && (
-        <div className="md:hidden absolute top-full w-full bg-gray-800 text-white">
-          <ul className="p-4">
-            <li className="mb-2">
-              <a href="/" className="hover:text-gray-300 block">
-                Home
-              </a>
-            </li>
-            <li className="mb-2">
-              <a href="/sports" className="hover:text-gray-300 block">
-                Sport News
-              </a>
-            </li>
-            <li className="mb-2">
-              <a href="/entertainment" className="hover:text-gray-300 block">
-                Entertainment News
-              </a>
-            </li>
-          </ul>
+        <div
+          className="fixed top-0 left-0 z-50 w-full h-full bg-black bg-opacity-90 flex flex-col"
+          onClick={closeMobileMenu}
+        >
+          <div className="flex-grow" onClick={(e) => e.stopPropagation()}>
+            <div className="flex justify-end">
+              <button
+                onClick={toggleMobileMenu}
+                className="p-4 focus:outline-none "
+              >
+                <FaTimes className="h-7 w-7 fill-white" />
+              </button>
+            </div>
+            <ul className="p-4 text-center">
+              <li className="mb-2">
+                <Link href="/" className="hover:text-gray-300 text-white block">
+                  Home
+                </Link>
+              </li>
+              <li className="mb-2">
+                <Link
+                  href="/sports"
+                  className="hover:text-gray-300 text-white block"
+                >
+                  Sport News
+                </Link>
+              </li>
+              <li className="mb-2">
+                <Link
+                  href="/entertainment"
+                  className="hover:text-gray-300 text-white block"
+                >
+                  Entertainment News
+                </Link>
+              </li>
+              <li className="mb-2">
+                <Link
+                  className="hover:text-gray-300 text-white block"
+                  href="/bookmarks"
+                >
+                  Bookmarks
+                </Link>
+              </li>
+            </ul>
+          </div>
         </div>
       )}
     </header>
