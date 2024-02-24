65be4000116a2fbfc2f61562106a4095fe0f5c80
ozayo <ozay.ozdemir@gmail.com>
2024-02-24 16:42:33 +0100

    state for dark mode

---
+++ src/store/darkModeSlice.js
@@ -0,0 +1,17 @@
+import { createSlice } from "@reduxjs/toolkit";
+
+export const darkModeSlice = createSlice({
+  name: "darkMode",
+  initialState: {
+    value: false,
+  },
+  reducers: {
+    toggleDarkMode: (state) => {
+      state.value = !state.value;
+    },
+  },
+});
+
+export const { toggleDarkMode } = darkModeSlice.actions;
+
+export default darkModeSlice.reducer;
