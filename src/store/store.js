a7ab36619ce4dce52d4d656b338bca9ec1f3b06c
ozayo <ozay.ozdemir@gmail.com>
2024-02-24 16:44:57 +0100

    dark mode state added

--- src/store/store.js
+++ src/store/store.js
@@ -1,9 +1,11 @@
 import { configureStore } from "@reduxjs/toolkit";
 import bookmarksReducer from "./bookmarkSlice";
+import darkModeReducer from "./darkModeSlice";
 
 export const store = configureStore({
-	// localStorage.setItem("items", JSON.stringify(state.items))
-	reducer: {
-		bookmarks: bookmarksReducer,
-	},
+  // localStorage.setItem("items", JSON.stringify(state.items))
+  reducer: {
+    bookmarks: bookmarksReducer,
+    darkMode: darkModeReducer,
+  },
 });
