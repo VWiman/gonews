dcfb06b811b8c51ed577ebac545f7b92644199f6
marwal1987 <marwal1987@gmail.com>
2024-02-26 17:42:01 +0100

    Styled bookmarks page

--- src/components/AccordionItem.js
+++ src/components/AccordionItem.js
@@ -1,14 +1,9 @@
 import { useState } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { toggleBookmark } from "@/store/bookmarkSlice";
+import NewsContent from "@/components/NewsContent";
 
-import {
-  FaArrowAltCircleDown,
-  FaSortAlphaUp,
-  FaSortDown,
-  FaSortUp,
-  FaUps,
-} from "react-icons/fa";
+import { FaSortDown, FaSortUp, FaStar, FaRegStar } from "react-icons/fa";
 import { IoMdClose } from "react-icons/io";
 const AccordionItem = ({ article }) => {
   const [openAccor, setOpenAccor] = useState(false);
@@ -21,52 +16,57 @@ const AccordionItem = ({ article }) => {
   };
 
   return (
-    <li key={article.article_id} className=" hover:bg-slate-100 p-2 rounded-lg">
-      <div
-        className="flex justify-between cursor-pointer"
-        onClick={() => setOpenAccor(!openAccor)}
+    <div className="gonews">
+      <li
+        key={article.article_id}
+        className=" hover:bg-slate-100 p-2 rounded-lg"
       >
-        <div>
-          <h2 className="text-2xl m-0">{article.title}</h2>
-        </div>
-        <div className=" cursor-pointer">
-          {!openAccor ? (
-            <FaSortDown className="h-5 text-black w-5 hover:cursor-pointer  " />
-          ) : (
-            <FaSortUp className="h-5 text-black w-5 hover:cursor-pointer mt-1" />
-          )}
-        </div>
-      </div>
-
-      <div
-        className={openAccor ? " flex flex-col md:flex-row py-4" : " hidden"}
-      >
-        <div className="max-h-[150px] max-w-[200px] ">
-          <img
-            className=" h-full w-full object-contain object-top"
-            src={article.image_url || defaultImg}
-            alt={article.title}
-            width={1000}
-          />
-        </div>
-        <div className=" px-5 ">{article.description}</div>
-      </div>
-
-      <button onClick={() => handleBookmarkToggle(article)}>
-        {bookmarks.list.some(
-          (bookmark) => bookmark.article_id === article.article_id
-        ) ? (
-          <div className=" flex items-center space-x-2 py-3 group ">
-            <IoMdClose className=" bg-red-500 rounded-full h-5 p-1 w-5 group-hover:opacity-75 " />
-            <p className=" group-hover:opacity-75 flex p-0  ">
-              Ta bort bokmärke
-            </p>
+        <div
+          className="flex justify-between cursor-pointer"
+          onClick={() => setOpenAccor(!openAccor)}
+        >
+          <div>
+            <h2 className="text-2xl m-0">{article.title}</h2>
           </div>
-        ) : (
-          "Lägg till bokmärke"
-        )}
-      </button>
-    </li>
+          <div className=" cursor-pointer">
+            {!openAccor ? (
+              <FaSortDown className="h-5 text-black w-5 hover:cursor-pointer  " />
+            ) : (
+              <FaSortUp className="h-5 text-black w-5 hover:cursor-pointer mt-1" />
+            )}
+          </div>
+        </div>
+
+        <div className={openAccor ? " newsblock " : " hidden"}>
+          <div className="newsimg">
+            <img
+              src={article.image_url || defaultImg}
+              alt={article.title}
+              width={200}
+            />
+          </div>
+          <div className="newsart">
+            <NewsContent link={article.link}>{article}</NewsContent>
+          </div>
+        </div>
+
+        <div className="newslinks">
+          <button onClick={() => handleBookmarkToggle(article)}>
+            {bookmarks.list.some(
+              (bookmark) => bookmark.article_id === article.article_id
+            ) ? (
+              <>
+                <FaStar /> Remove bookmark
+              </>
+            ) : (
+              <>
+                <FaRegStar /> Add bookmark
+              </>
+            )}
+          </button>
+        </div>
+      </li>
+    </div>
   );
 };
 
--- src/pages/entertainment.js
+++ src/pages/entertainment.js
@@ -8,80 +8,91 @@ import NewsContent from "@/components/NewsContent";
 
 // definiera en funktionell komponent Entertainment, som tar emot nyhetsdata som en prop
 const Entertainment = ({ news }) => {
-	const defaultImg = "/images/default.jpeg";
-	//hämta bokmärken från Redux-store och lagra dem i variabeln bookmarks
-	const bookmarks = useSelector((state) => state.bookmarks);
+  const defaultImg = "/images/default.jpeg";
+  //hämta bokmärken från Redux-store och lagra dem i variabeln bookmarks
+  const bookmarks = useSelector((state) => state.bookmarks);
 
-	// hook för att få en actionfunktion från Redux och lagra den i variabeln dispatch
-	const dispatch = useDispatch();
+  // hook för att få en actionfunktion från Redux och lagra den i variabeln dispatch
+  const dispatch = useDispatch();
 
-	// hantera klickhändelsen för att lägga till eller ta bort ett bokmärke.
-	const handleBookmarkToggle = (article) => {
-		dispatch(toggleBookmark(article));
-	};
+  // hantera klickhändelsen för att lägga till eller ta bort ett bokmärke.
+  const handleBookmarkToggle = (article) => {
+    dispatch(toggleBookmark(article));
+  };
 
-	// rendera nyhetsartiklarna
-	return (
-		<div>
-			<h1>Entertainment News</h1>
-			<div className="gonews">
-				{news.map((article) => (
-					<div key={article.article_id} className="newsblock">
-						<div className="newsimg">
-							<img src={article.image_url || defaultImg} alt={article.title} width={200} />
-						</div>
-						<div className="newsart">
-							<NewsContent link={article.link}>{article}</NewsContent>
-							<div className="newslinks">
-								<div>
-									{/* Added some() function for adding class in button when is added */}
-									<button
-										onClick={() => handleBookmarkToggle(article)}
-										className={`button ${
-											bookmarks.list.some((bookmark) => bookmark.article_id === article.article_id) ? "added" : ""
-										}`}>
-										{bookmarks.list.some((bookmark) => bookmark.article_id === article.article_id) ? (
-											<>
-												<FaStar /> Remove bookmark
-											</>
-										) : (
-											<>
-												<FaRegStar /> Add bookmark
-											</>
-										)}
-									</button>
-								</div>
-								<div>
-									<Link href={`/article/entertainment/${article.article_id}`}>
-										Read more <FaAngleDoubleRight />
-									</Link>
-								</div>
-							</div>
-						</div>
-					</div>
-				))}
-			</div>
-		</div>
-	);
+  // rendera nyhetsartiklarna
+  return (
+    <div>
+      <h1>Entertainment News</h1>
+      <div className="gonews">
+        {news.map((article) => (
+          <div key={article.article_id} className="newsblock">
+            <div className="newsimg">
+              <img
+                src={article.image_url || defaultImg}
+                alt={article.title}
+                width={200}
+              />
+            </div>
+            <div className="newsart">
+              <NewsContent link={article.link}>{article}</NewsContent>
+              <div className="newslinks">
+                <div>
+                  {/* Added some() function for adding class in button when is added */}
+                  <button
+                    onClick={() => handleBookmarkToggle(article)}
+                    className={`button ${
+                      bookmarks.list.some(
+                        (bookmark) => bookmark.article_id === article.article_id
+                      )
+                        ? "added"
+                        : ""
+                    }`}
+                  >
+                    {bookmarks.list.some(
+                      (bookmark) => bookmark.article_id === article.article_id
+                    ) ? (
+                      <>
+                        <FaStar /> Remove bookmark
+                      </>
+                    ) : (
+                      <>
+                        <FaRegStar /> Add bookmark
+                      </>
+                    )}
+                  </button>
+                </div>
+                <div>
+                  <Link href={`/article/entertainment/${article.article_id}`}>
+                    Read more <FaAngleDoubleRight />
+                  </Link>
+                </div>
+              </div>
+            </div>
+          </div>
+        ))}
+      </div>
+    </div>
+  );
 };
 
 // funktion för att hämta nyhetsdata vid build.
 export async function getStaticProps() {
-	const apiKey = process.env.API_KEY;
-	const options = `category=entertainment&size=10&language=en`;
+  const apiKey = process.env.API_KEY;
+  const options = `category=entertainment&size=10&language=en`;
 
-	try {
-		const news = await fetchNews(apiKey, options);
+  try {
+    const news = await fetchNews(apiKey, options);
 
-		return {
-			props: { news },
-		};
-	} catch (error) {
-		console.error("Error fetching news:", error);
-		return {
-			props: { news: [] },
-		};
-	}
+    return {
+      props: { news },
+    };
+  } catch (error) {
+    console.error("Error fetching news:", error);
+    return {
+      props: { news: [] },
+    };
+  }
 }
 
 export default Entertainment;
