c47e9d4e1b533df3e2d92c209b94e83808952480
ozayo <ozay.ozdemir@gmail.com>
2024-02-26 16:14:20 +0100

    fixed error & added new logo

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,13 +1,13 @@
-// Header.js
-
 import { useState } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { toggleDarkMode } from "@/store/darkModeSlice";
 import { IoMdMoon, IoMdSunny } from "react-icons/io";
 import { FaBars, FaTimes } from "react-icons/fa";
 import Link from "next/link";
+import { useRouter } from "next/router";
 
 const Header = () => {
+  const router = useRouter();
   const dispatch = useDispatch();
   const darkMode = useSelector((state) => state.darkMode.value);
   const [isMobileMenuOpen, setIsMobileMenuOpen] = useState(false);
@@ -29,18 +29,58 @@ const Header = () => {
             <img
               src={darkMode ? "/images/gonews-w.svg" : "/images/gonews.svg"}
               alt="Logo"
-              className="h-9 w-auto mr-4"
+              className="h-12 w-auto mr-4"
               width={140}
             />
           </Link>
         </div>
 
         {/* Desktop navigation */}
-        <nav className="hidden md:flex space-x-4">
-          <Link href="/">Home</Link>
-          <Link href="/sports">Sports</Link>
-          <Link href="/entertainment">Entertainment</Link>
-          <Link href="/bookmarks">Bookmarks</Link>
+        <nav className="desknav hidden md:flex space-x-4">
+          <Link
+            href="/"
+            onClick={(e) => {
+              if (router.pathname === "/") {
+                e.preventDefault();
+              }
+            }}
+            className={router.pathname === "/" ? "current" : ""}
+          >
+            Home
+          </Link>
+          <Link
+            href="/sports"
+            onClick={(e) => {
+              if (router.pathname === "/sports") {
+                e.preventDefault();
+              }
+            }}
+            className={router.pathname === "/sports" ? "current" : ""}
+          >
+            Sports
+          </Link>
+          <Link
+            href="/entertainment"
+            onClick={(e) => {
+              if (router.pathname === "/entertainment") {
+                e.preventDefault();
+              }
+            }}
+            className={router.pathname === "/entertainment" ? "current" : ""}
+          >
+            Entertainment
+          </Link>
+          <Link
+            href="/bookmarks"
+            onClick={(e) => {
+              if (router.pathname === "/bookmarks") {
+                e.preventDefault();
+              }
+            }}
+            className={router.pathname === "/bookmarks" ? "current" : ""}
+          >
+            Bookmarks
+          </Link>
         </nav>
 
         {/* Dark mode toggle */}
@@ -84,16 +124,29 @@ const Header = () => {
                 <FaTimes className="h-7 w-7 fill-white" />
               </button>
             </div>
-            <ul className="p-4 text-center">
+            <ul className="mobilnav p-4 text-center">
               <li className="mb-2">
-                <Link href="/" className="hover:text-gray-300 text-white block">
+                <Link
+                  href="/"
+                  onClick={(e) => {
+                    if (router.pathname === "/") {
+                      e.preventDefault();
+                    }
+                  }}
+                  className={router.pathname === "/" ? "current" : ""}
+                >
                   Home
                 </Link>
               </li>
               <li className="mb-2">
                 <Link
                   href="/sports"
-                  className="hover:text-gray-300 text-white block"
+                  onClick={(e) => {
+                    if (router.pathname === "/sports") {
+                      e.preventDefault();
+                    }
+                  }}
+                  className={router.pathname === "/sports" ? "current" : ""}
                 >
                   Sports
                 </Link>
@@ -101,15 +154,27 @@ const Header = () => {
               <li className="mb-2">
                 <Link
                   href="/entertainment"
-                  className="hover:text-gray-300 text-white block"
+                  onClick={(e) => {
+                    if (router.pathname === "/entertainment") {
+                      e.preventDefault();
+                    }
+                  }}
+                  className={
+                    router.pathname === "/entertainment" ? "current" : ""
+                  }
                 >
                   Entertainment
                 </Link>
               </li>
               <li className="mb-2">
                 <Link
-                  className="hover:text-gray-300 text-white block"
                   href="/bookmarks"
+                  onClick={(e) => {
+                    if (router.pathname === "/bookmarks") {
+                      e.preventDefault();
+                    }
+                  }}
+                  className={router.pathname === "/bookmarks" ? "current" : ""}
                 >
                   Bookmarks
                 </Link>
