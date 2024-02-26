b44731d4488b4932426e77586cd9c841b4221ee7
marwal1987 <marwal1987@gmail.com>
2024-02-26 08:11:02 +0100

    Removed temp-nav in bookmarks.js

--- src/pages/bookmarks.js
+++ src/pages/bookmarks.js
@@ -1,6 +1,4 @@
 import AccordionItem from "@/components/AccordionItem";
-import Link from "next/link";
-
 import { useSelector } from "react-redux";
 
 const Bookmarks = () => {
@@ -8,16 +6,7 @@ const Bookmarks = () => {
 
   return (
     <div>
-      <div className=" bg-blue-200">
-        <p>Temp navbar</p>
-        <Link href="/">Home</Link>
-        <hr />
-        <Link href="/sports">Sports</Link>
-        <hr />
-        <Link href="/entertainment">Entertainment</Link>
-      </div>
       <h2>Bookmarks</h2>
-
       <ul className=" space-y-4">
         {/* // Add accordian and show description and image on click article.description och article.image_url  */}
         {bookmarks.list.map((article) => (
